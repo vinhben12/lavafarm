c9553b3ae0d75cb352ca8b2efec10fda41fd7c9a
Jaden <75365338+vinhben12@users.noreply.github.com>
2024-04-15 00:09:42 +0700

    axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-da0d19f9-f04b-44ac-a91b-2efeff320aaa/"
 
 OUTPUT_DIR="logs"
 
